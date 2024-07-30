27865c83687524d387bf364810fb009dcea0f4d9
AlissonPaez <alisson.paez@uptc.edu.co>
2024-07-29 23:37:03 -0500

    Añadir venta

---
+++ ManagementSaleHTML/js/addsale.js
@@ -0,0 +1,40 @@
+function addSale(){
+    let code = document.getElementById("input-code-sale").value;
+    let date = document.getElementById("input-date-sale").value;
+    let numberProductsSold = document.getElementById("input-number-products-sold").value;
+    let paymentMethod = document.getElementById("input-payment-method").value;
+    let saleStatus= document.getElementById("input-sale-status").value;
+    let totalSale = document.getElementById("input-total-sale").value;
+
+    let saleData = {
+        code: code,
+        date: date,
+        numberProductsSold: numberProductsSold,
+        paymentMethod: paymentMethod,
+        yearPublish: yearPublish,
+        saleStatus: saleStatus,
+        totalSale: totalSale
+    };
+
+    let url = 'http://localhost:8080/ManagementSale/rest/ManagementSale/createSale';
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(saleData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Ocurrió un error en la respuesta del servidor: ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert("Se agregó el registro.");
+        window.location.href = "./dashboard.html";
+    })
+    .catch(error => {
+        console.error('Ocurrió el siguiente error con la operación: ', error);
+    });
+}
--- ManagementSaleHTML/js/script.js
+++ ManagementSaleHTML/js/script.js
@@ -5,7 +5,7 @@ function validateUser(){
     /* Propiedad para redirigir */
     //window.location.href = "nueva_pagina.html";
 
-    fetch('http://localhost:8080/ManagementSale/ManagementSale/rest/ManagementUser/validateUser?nameUser=' +userName +'&password='+password)
+    fetch('http://localhost:8080/ManagementSale/rest/ManagementUser/validateUser?nameUser=' +userName +'&password='+password)
     .then(response => response.json())
     .then(data => {
         window.location.href = "./dashboard.html";
