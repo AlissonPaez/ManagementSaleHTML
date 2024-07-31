5a3b9953a61fa5acd54cf1ae4f64f8409a04de1c
AlissonPaez <alisson.paez@uptc.edu.co>
2024-07-30 20:03:05 -0500

    product

---
+++ ManagementSaleHTML/js/updatesale.js
@@ -0,0 +1,54 @@
+let codeSale = "";
+function loadPageSale(){
+    let localStorageSale = localStorage.getItem("saleData");;
+    let saleData = JSON.parse(localStorageSale);
+    codeSale = saleData.code;
+    document.getElementById("input-date-sale").value = saleData.date;
+    document.getElementById("input-number-products-sold").value = saleData.numberProductsSold;
+    document.getElementById("input-payment-method").value = saleData.paymentMethod;
+    document.getElementById("input-sale-status").value = saleData.saleStatus;
+    document.getElementById("input-total-sale").value = saleData.totalSale;
+    document.getElementById("input-code-product").value = saleData.codeProduct;
+}
+loadPageSale();
+
+function updateSale(){
+    let date = document.getElementById("input-date-sale").value;
+    let numberProductsSold = document.getElementById("input-number-products-sold").value;
+    let paymentMethod = document.getElementById("input-payment-method").value;
+    let saleStatus = document.getElementById("input-sale-status").value;
+    let totalSale = document.getElementById("input-total-sale").value;
+    let codeProduct= document.getElementById("input-code-product").value;
+
+    let saleData = {
+        code: codeSale,
+        date: date,
+        numberProductsSold: numberProductsSold,
+        paymentMethod: paymentMethod,
+        saleStatus: saleStatus,
+        totalSale: totalSale,
+        codeProduct: codeProduct
+    };
+
+    let url = 'http://localhost:8080/Library/rest/ManagementLibrary/updateBookAttribute';
+    fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(saleData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Ocurrió un error en la respuesta del servidor: ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert("Se actualizó el registro.");
+        window.location.href = "./dashboard.html";
+    })
+    .catch(error => {
+        console.error('Ocurrió el siguiente error con la operación: ', error);
+    });
+}
